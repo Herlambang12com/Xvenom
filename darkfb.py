97dba74fdd58fb63213022469f60ac60b730c394
H.A.Kvortex <57755112+Herlambang12com@users.noreply.github.com>
2019-12-01 14:56:29 +0700

    Add files via upload

---
+++ darkfb.py
@@ -0,0 +1,38 @@
+logo = """
+\x1b[1;32m █████████
+\x1b[1;32m █▄█████▄█          \x1b[1;32m ●▬▬▬▬▬▬▬▬▬▬▬▬๑۩۩๑▬▬▬▬▬▬▬▬▬▬▬●
+\x1b[1;32m █▼▼▼▼▼▼▼  -_ --_--  \x1b[1;36m    ╔╦╗┌─┐┬─┐┬┌─   ╔═╗╔╗
+\x1b[1;32m █    _-_-- -_ --__  \x1b[1;36m     ║║├─┤├┬┘├┴┐───╠╣ ╠╩╗
+\x1b[1;32m █▲▲▲▲▲▲▲  - - _ --  \x1b[1;36m    ═╩╝┴ ┴┴└─┴ ┴   ╚  ╚═╝  \x1b[1;33mVIP
+\x1b[1;32m █████████         \x1b[1;32m«===============✧==============»
+\x1b[1;32m  ██ ██
+\x1b[00m ╔══════════════════════════════════════════════════╗
+\x1b[00m ║\x1b[1;33m * \x1b[00m Author  : \x1b[1;36m PRQUINO                          \x1b[00m║
+\x1b[00m ║\x1b[1;33m * \x1b[00m Youtube : \x1b[1;36m PRQUINO                             \x1b[00m║
+\x1b[00m ║\x1b[1;33m * \x1b[00m Github  : \x1b[1;36m https://github.com/Herlambang12com          \x1b[00m║
+\x1b[00m ╚══════════════════════════════════════════════════╝
+"""
+def menu():
+	os.system('clear')
+	print logo
+	print('')
+	print('\x1b[1;32mWELCOME TO DARK FACEBOOK LOGIN')
+	print('\x1b[00mAutomatic Cracking Facebook ! \033[041mVIP\033[00m')
+	print('\x1b[00mPlease login with Facebook Account !')
+	print('')
+	main()
+
+def main():
+	id = raw_input('Username : ')
+	pw = raw_input('Password : ')
+	print('')
+	os.system('sleep 5')
+	print('\x1b[1;91m[!] Your account is CheckPoint\x1b[00m')
+	print('\x1b[1;91m[!] Please login into your browser first !')
+	print('\x1b[1;91m[!] And try to login again ...')
+	os.system('sleep 10')
+	os.system('sleep 10')
+	os.system('clear')
+	menu()
+
+menu()
